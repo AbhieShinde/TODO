311ef27d1a6819906c88d9cf543b3da7cfa7fc1a
ashinde <shindea890@gmail.com>
2021-02-14 01:20:32 +0530

    Submit on Enter & Cleaner UI - Icon Changed
    Signed-off-by: ashinde <shindea890@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -85,15 +84,23 @@ class App extends React.Component {
     localStorage.clear();
   }
 
+  submitOnEnter( keyCode ) {
+    if( 13 == keyCode ) {
+      this.addItem(this.state.newItem);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <div className="pb-4">
+            <img src="https://img.icons8.com/color/96/000000/todo-list--v2.png"/>
+          </div>
           <div className="list-group pb-4">
-          <div className="input-group input-group-lg">
+          <div className="input-group input-group-lg gap-2">
             <button className="btn btn-outline-danger" type="button" onClick={() => this.clearAll()} disabled={!this.state.list.length} ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-journal-x mx-auto" viewBox="0 0 16 16"><path fillRule="evenodd" d="M6.146 6.146a.5.5 0 0 1 .708 0L8 7.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 8l1.147 1.146a.5.5 0 0 1-.708.708L8 8.707 6.854 9.854a.5.5 0 0 1-.708-.708L7.293 8 6.146 6.854a.5.5 0 0 1 0-.708z"/><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/></svg> Clear All</button>
-            <input type="text" className="form-control" placeholder="Add new TODO" aria-label="TODO" aria-describedby="basic-addon1" required value={this.state.newItem} onChange={e => this.updateInput(e.target.value)} />
+            <input type="text" className="form-control text-center bg-dark text-info" placeholder="Type TODO" aria-label="TODO" aria-describedby="basic-addon1" onKeyUp={e => this.submitOnEnter(e.keyCode)} required value={this.state.newItem} onChange={e => this.updateInput(e.target.value)} />
             <button type="button" className="btn btn-outline-info" id="button-addon2" onClick={() => this.addItem(this.state.newItem)} disabled={!this.state.newItem.length} >Insert <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-journal-plus" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/></svg></button>
           </div>
           </div>
